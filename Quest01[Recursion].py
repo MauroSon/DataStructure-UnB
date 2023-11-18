25ab372c5d79acc188623f1558c52c5bbf5e4b73
MauroSon <127454440+MauroSon@users.noreply.github.com>
2023-11-18 18:03:38 -0300

    Update Quest01[Recursion].py

--- Quest01[Recursion].py
+++ Quest01[Recursion].py
@@ -1,3 +1,14 @@
+'''
+Can you remember the problem that I discussed in the earlier classes?
+
+If not, do not worry, read it now carefully.
+
+One day, coco-cola offered a special campaign. If you return three empty bottles to the shop, you will get a full bottle of coco-cola to drink. If you have n empty bottles right in your hand, how many full bottles of coco-cola can you drink?
+
+There will be at most 10 test cases, each containing a single line with an integer n (0 < n < 101). The input terminates with n = 0, which should not be processed.
+
+For each test case, print the number of full bottles of coco-cola that you can drink.
+'''
 def drink(i,cont):
     if i==1:
         return cont
